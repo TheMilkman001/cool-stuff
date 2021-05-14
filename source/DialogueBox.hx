2f2ca3382583596fce66411ebef7fcd44bcc28f2
TheMilkman001 <StrikrrT@outlook.com>
2021-05-14 14:31:02 -0700

    let's go

---
+++ source/DialogueBox.hx
@@ -0,0 +1,418 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.addons.text.FlxTypeText;
+import flixel.graphics.frames.FlxAtlasFrames;
+import flixel.group.FlxSpriteGroup;
+import flixel.input.FlxKeyManager;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import flixel.util.FlxTimer;
+
+using StringTools;
+
+class DialogueBox extends FlxSpriteGroup
+{
+	var box:FlxSprite;
+
+	var curCharacter:String = '';
+
+	var dialogue:Alphabet;
+	var dialogueList:Array<String> = [];
+
+	// SECOND DIALOGUE FOR THE PIXEL SHIT INSTEAD???
+	var swagDialogue:FlxTypeText;
+
+	var dropText:FlxText;
+
+	public var finishThing:Void->Void;
+
+	var portraitLeft:FlxSprite;
+	var portraitLeftlemondemon:FlxSprite;
+	var portraitLeftskid:FlxSprite;
+	var portraitLeftpump:FlxSprite;
+	var portraitLeftsenpai:FlxSprite;
+	var portraitRight:FlxSprite;
+	var portraitRightbf:FlxSprite;
+	var portraitRightbfpixel:FlxSprite;
+
+	var handSelect:FlxSprite;
+	var bgFade:FlxSprite;
+
+	public function new(talkingRight:Bool = true, ?dialogueList:Array<String>)
+	{
+		super();
+
+		switch (PlayState.SONG.song.toLowerCase())
+		{
+			case 'senpai':
+				FlxG.sound.playMusic(Paths.music('Lunchbox'), 0);
+				FlxG.sound.music.fadeIn(1, 0, 0.8);
+			case 'thorns':
+				FlxG.sound.playMusic(Paths.music('LunchboxScary'), 0);
+				FlxG.sound.music.fadeIn(1, 0, 0.8);
+		}
+
+		bgFade = new FlxSprite(-200, -200).makeGraphic(Std.int(FlxG.width * 1.3), Std.int(FlxG.height * 1.3), 0xFFB3DFd8);
+		bgFade.scrollFactor.set();
+		bgFade.alpha = 0;
+		add(bgFade);
+
+		new FlxTimer().start(0.83, function(tmr:FlxTimer)
+		{
+			bgFade.alpha += (1 / 5) * 0.7;
+			if (bgFade.alpha > 0.7)
+				bgFade.alpha = 0.7;
+		}, 5);
+
+		var hasDialog = false;
+		if (PlayState.SONG.song.toLowerCase()=='senpai' || PlayState.SONG.song.toLowerCase()=='roses' || PlayState.SONG.song.toLowerCase()=='thorns')
+		{
+		box = new FlxSprite(-20, 45);
+		
+		switch (PlayState.SONG.song.toLowerCase())
+		{
+			case 'senpai':
+				hasDialog = true;
+				box.frames = Paths.getSparrowAtlas('weeb/pixelUI/dialogueBox-pixel');
+				box.animation.addByPrefix('normalOpen', 'Text Box Appear', 24, false);
+				box.animation.addByIndices('normal', 'Text Box Appear', [4], "", 24);
+			case 'roses':
+				hasDialog = true;
+				FlxG.sound.play(Paths.sound('ANGRY_TEXT_BOX'));
+
+				box.frames = Paths.getSparrowAtlas('weeb/pixelUI/dialogueBox-senpaiMad');
+				box.animation.addByPrefix('normalOpen', 'SENPAI ANGRY IMPACT SPEECH', 24, false);
+				box.animation.addByIndices('normal', 'SENPAI ANGRY IMPACT SPEECH', [4], "", 24);
+
+			case 'thorns':
+				hasDialog = true;
+				box.frames = Paths.getSparrowAtlas('weeb/pixelUI/dialogueBox-evil');
+				box.animation.addByPrefix('normalOpen', 'Spirit Textbox spawn', 24, false);
+				box.animation.addByIndices('normal', 'Spirit Textbox spawn', [11], "", 24);
+
+				var face:FlxSprite = new FlxSprite(320, 170).loadGraphic(Paths.image('weeb/spiritFaceForward'));
+				face.setGraphicSize(Std.int(face.width * 6));
+				add(face);
+		}
+		}
+		else
+		{
+			box = new FlxSprite(-20, 395);
+		
+		switch (PlayState.SONG.song.toLowerCase())
+		{	
+			case 'spooky-month' | 'spooky month' | 'touch-tone-telephone' | 'touch tone telephone':
+				hasDialog = true;
+				box.frames = Paths.getSparrowAtlas('portraits/speech_bubble_talking');
+				box.animation.addByPrefix('normalOpen', 'Speech Bubble Normal Open', 24, false);
+				box.animation.addByIndices('normal', 'speech bubble normal', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19], "", 24);
+		}
+		}
+
+		this.dialogueList = dialogueList;
+		
+		if (!hasDialog)
+			return;
+		portraitLeft = new FlxSprite(-20, 40);
+		portraitLeft.frames = Paths.getSparrowAtlas('weeb/senpaiPortrait');
+		portraitLeft.animation.addByPrefix('enter', 'Senpai Portrait Enter instance 1', 24, false);
+		portraitLeft.setGraphicSize(Std.int(portraitLeft.width * PlayState.daPixelZoom * 0.9));
+		portraitLeft.updateHitbox();
+		portraitLeft.scrollFactor.set();
+		add(portraitLeft);
+		portraitLeft.visible = false;
+
+		portraitRight = new FlxSprite(0, 40);
+		portraitRight.frames = Paths.getSparrowAtlas('weeb/bfPortrait');
+		portraitRight.animation.addByPrefix('enter', 'Boyfriend portrait enter instance 1', 24, false);
+		portraitRight.setGraphicSize(Std.int(portraitRight.width * PlayState.daPixelZoom * 0.9));
+		portraitRight.updateHitbox();
+		portraitRight.scrollFactor.set();
+		add(portraitRight);
+		portraitRight.visible = false;
+
+		portraitLeftskid = new FlxSprite(-200, 40);
+		portraitLeftskid.frames = Paths.getSparrowAtlas('portraits/skidPortrait');
+		portraitLeftskid.animation.addByPrefix('enter', 'Portrait Enter instance 1', 24, false);
+		portraitLeftskid.updateHitbox();
+		portraitLeftskid.scrollFactor.set();
+		add(portraitLeftskid);
+		portraitLeftskid.visible = false;
+
+		portraitLeftpump = new FlxSprite(-200, 40);
+		portraitLeftpump.frames = Paths.getSparrowAtlas('portraits/pumpPortrait');
+		portraitLeftpump.animation.addByPrefix('enter', 'Portrait Enter instance 1', 24, false);
+		portraitLeftpump.updateHitbox();
+		portraitLeftpump.scrollFactor.set();
+		add(portraitLeftpump);
+		portraitLeftpump.visible = false;
+
+		portraitLeftlemondemon = new FlxSprite(-200, 40);
+		portraitLeftlemondemon.frames = Paths.getSparrowAtlas('portraits/christmasLemonPortrait');
+		portraitLeftlemondemon.animation.addByPrefix('enter', 'Portrait Enter instance 1', 24, false);
+		portraitLeftlemondemon.updateHitbox();
+		portraitLeftlemondemon.scrollFactor.set();
+		add(portraitLeftlemondemon);
+		portraitLeftlemondemon.visible = false;
+		
+		portraitLeftsenpai = new FlxSprite(-20, 40);
+		portraitLeftsenpai.frames = Paths.getSparrowAtlas('weeb/senpaiPortrait');
+		portraitLeftsenpai.animation.addByPrefix('enter', 'Senpai Portrait Enter instance 1', 24, false);
+		portraitLeftsenpai.setGraphicSize(Std.int(portraitLeft.width * PlayState.daPixelZoom * 0.9));
+		portraitLeftsenpai.updateHitbox();
+		portraitLeftsenpai.scrollFactor.set();
+		add(portraitLeftsenpai);
+		portraitLeftsenpai.visible = false;
+
+		portraitRightbf = new FlxSprite(10, 50);
+		portraitRightbf.frames = Paths.getSparrowAtlas('portraits/boyfriendPortrait');
+		portraitRightbf.animation.addByPrefix('enter', 'Portrait Enter instance 1', 24, false);
+		portraitRightbf.updateHitbox();
+		portraitRightbf.scrollFactor.set();
+		add(portraitRightbf);
+		portraitRightbf.visible = false;
+		
+		if (PlayState.SONG.song.toLowerCase()=='senpai' || PlayState.SONG.song.toLowerCase()=='roses' || PlayState.SONG.song.toLowerCase()=='thorns')
+		{
+		box.animation.play('normalOpen');
+		box.setGraphicSize(Std.int(box.width * PlayState.daPixelZoom * 0.9));
+		box.updateHitbox();
+		add(box);
+		}
+
+		else
+		{
+			box.animation.play('normalOpen');
+			box.updateHitbox();
+			add(box);	
+		}
+		if (PlayState.SONG.song.toLowerCase()=='senpai' || PlayState.SONG.song.toLowerCase()=='roses' || PlayState.SONG.song.toLowerCase()=='thorns')
+		{
+		portraitLeft.screenCenter(X);
+		box.screenCenter(X);
+		}
+		else
+		{
+			box.screenCenter(X);
+		}
+		
+		handSelect = new FlxSprite(FlxG.width * 0.9, FlxG.height * 0.9).loadGraphic(Paths.image('weeb/pixelUI/hand_textbox'));
+		add(handSelect);
+
+
+		if (!talkingRight)
+		{
+			// box.flipX = true;
+		}
+		
+		if (PlayState.SONG.song.toLowerCase()=='senpai' || PlayState.SONG.song.toLowerCase()=='roses' || PlayState.SONG.song.toLowerCase()=='thorns')
+		{
+		dropText = new FlxText(242, 502, Std.int(FlxG.width * 0.6), "", 32);
+		dropText.font = 'Pixel Arial 11 Bold';
+		dropText.color = 0xFFD89494;
+		add(dropText);
+
+		swagDialogue = new FlxTypeText(240, 500, Std.int(FlxG.width * 0.6), "", 32);
+		swagDialogue.font = 'Pixel Arial 11 Bold';
+		swagDialogue.color = 0xFF3F2021;
+		swagDialogue.sounds = [FlxG.sound.load(Paths.sound('pixelText'), 0.6)];
+		add(swagDialogue);
+		}
+		else
+		{
+		dropText = new FlxText(242, 502, Std.int(FlxG.width * 0.6), "", 32);
+		dropText.font = 'Adobe Blank';
+		dropText.color = 0xFFD89494;
+		add(dropText);
+
+		swagDialogue = new FlxTypeText(240, 500, Std.int(FlxG.width * 0.6), "", 32);
+		swagDialogue.font = 'Komika Display';
+		swagDialogue.color = 0xFF000000;
+		swagDialogue.sounds = [FlxG.sound.load(Paths.sound('pixelText'), 0.6)];
+		add(swagDialogue);
+		}
+
+		dialogue = new Alphabet(0, 80, "", false, true);
+		// dialogue.x = 90;
+		// add(dialogue);
+	}
+
+	var dialogueOpened:Bool = false;
+	var dialogueStarted:Bool = false;
+
+	override function update(elapsed:Float)
+	{
+		// HARD CODING CUZ IM STUPDI
+		if (PlayState.SONG.song.toLowerCase() == 'roses')
+			portraitLeft.visible = false;
+		if (PlayState.SONG.song.toLowerCase() == 'thorns')
+		{
+			portraitLeft.color = FlxColor.BLACK;
+			swagDialogue.color = FlxColor.WHITE;
+			dropText.color = FlxColor.BLACK;
+		}
+
+		dropText.text = swagDialogue.text;
+
+		if (box.animation.curAnim != null)
+		{
+			if (box.animation.curAnim.name == 'normalOpen' && box.animation.curAnim.finished)
+			{
+				box.animation.play('normal');
+				dialogueOpened = true;
+			}
+		}
+
+		if (dialogueOpened && !dialogueStarted)
+		{
+			startDialogue();
+			dialogueStarted = true;
+		}
+
+		if (FlxG.keys.justPressed.ANY  && dialogueStarted == true)
+		{
+			remove(dialogue);
+				
+			FlxG.sound.play(Paths.sound('clickText'), 0.8);
+
+			if (dialogueList[1] == null && dialogueList[0] != null)
+			{
+				if (!isEnding)
+				{
+					isEnding = true;
+
+					if (PlayState.SONG.song.toLowerCase() == 'senpai' || PlayState.SONG.song.toLowerCase() == 'thorns')
+						FlxG.sound.music.fadeOut(2.2, 0);
+
+					new FlxTimer().start(0.2, function(tmr:FlxTimer)
+					{
+						box.alpha -= 1 / 5;
+						bgFade.alpha -= 1 / 5 * 0.7;
+						portraitLeft.visible = false;
+						portraitRight.visible = false;
+						swagDialogue.alpha -= 1 / 5;
+						dropText.alpha = swagDialogue.alpha;
+					}, 5);
+
+					new FlxTimer().start(1.2, function(tmr:FlxTimer)
+					{
+						finishThing();
+						kill();
+					});
+				}
+			}
+			else
+			{
+				dialogueList.remove(dialogueList[0]);
+				startDialogue();
+			}
+		}
+		
+		super.update(elapsed);
+	}
+
+	var isEnding:Bool = false;
+
+	function startDialogue():Void
+	{
+		cleanDialog();
+		// var theDialog:Alphabet = new Alphabet(0, 70, dialogueList[0], false, true);
+		// dialogue = theDialog;
+		// add(theDialog);
+
+		// swagDialogue.text = ;
+		swagDialogue.resetText(dialogueList[0]);
+		swagDialogue.start(0.04, true);
+
+		switch (curCharacter)
+		{
+			case 'senpai':
+				portraitRight.visible = false;
+				portraitRightbf.visible = false;
+				portraitLeftskid.visible = false;
+				portraitLeftpump.visible = false;
+				portraitLeftlemondemon.visible = false;
+				if (!portraitLeft.visible)
+				{
+					portraitLeft.visible = true;
+					portraitLeft.animation.play('enter');
+				}
+			case 'lemondemon':
+				portraitRight.visible = false;
+				portraitRightbf.visible = false;
+				portraitLeftskid.visible = false;
+				portraitLeftpump.visible = false;
+				portraitLeft.visible = false;
+				if (!portraitLeftlemondemon.visible)
+				{
+					portraitLeftlemondemon.visible = true;
+					portraitLeftlemondemon.animation.play('enter');
+				}
+			case 'skid':
+				portraitRight.visible = false;
+				portraitRightbf.visible = false;
+				portraitLeft.visible = false;
+				portraitLeftpump.visible = false;
+				portraitLeftlemondemon.visible = false;
+				if (!portraitLeftskid.visible)
+				{
+					portraitLeftskid.visible = true;
+					portraitLeftskid.animation.play('enter');
+				}
+			case 'pump':
+				portraitRight.visible = false;
+				portraitRightbf.visible = false;
+				portraitLeft.visible = false;
+				portraitLeftskid.visible = false;
+				portraitLeftlemondemon.visible = false;
+				if (!portraitLeftpump.visible)
+				{
+					portraitLeftpump.visible = true;
+					portraitLeftpump.animation.play('enter');
+				}
+			case 'bfpixel':
+				portraitRightbf.visible = false;
+				portraitLeft.visible = false;
+				portraitLeftpump.visible = false;
+				portraitLeftskid.visible = false;
+				portraitLeftlemondemon.visible = false;
+				if (!portraitRight.visible)
+				{
+					portraitRight.visible = true;
+					portraitRight.animation.play('enter');
+				}
+			case 'bf':
+				portraitLeftpump.visible = false;
+				portraitLeftskid.visible = false;
+				portraitLeft.visible = false;
+				portraitRight.visible = false;
+				portraitLeftlemondemon.visible = false;
+				if (!portraitRightbf.visible)
+				{
+					portraitRightbf.visible = true;
+					portraitRightbf.animation.play('enter');
+				}
+			case 'blank':
+				portraitLeftpump.visible = false;
+				portraitLeftskid.visible = false;
+				portraitLeft.visible = false;
+				portraitRight.visible = false;
+				portraitRightbf.visible = false;
+				portraitLeftlemondemon.visible = false;
+				if (!portraitRightbf.visible)
+				{
+					portraitRightbf.visible = false;
+					portraitRightbf.animation.play('enter');
+				}
+		}
+	}
+
+	function cleanDialog():Void
+	{
+		var splitName:Array<String> = dialogueList[0].split(":");
+		curCharacter = splitName[1];
+		dialogueList[0] = dialogueList[0].substr(splitName[1].length + 2).trim();
+	}
+}
